fc4681d045261a6c19a8e7f18f66214a9ee9144e
Rafael Medeiros <tecnorafa12@gmail.com>
2025-07-27 22:05:40 +0000

    agora tá com esse erro: 2025-Jul-27 21:37:01.886502 ------ 2025-Jul-27 2

---
+++ next.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  transpilePackages: ["@radix-ui/react-slider"],
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
