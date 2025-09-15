91e558e33c96901b58621cbbe0c26f1f48dfe5ca
Rafael Medeiros <tecnorafa12@gmail.com>
2025-09-15 14:12:09 +0000

    precisamos arrumar aquele problema de seleção, aquele problema clássico,

--- src/components/turmas/edit-class-form.tsx
+++ src/components/turmas/edit-class-form.tsx
@@ -277,7 +277,7 @@ export function EditClassForm({ classData, children }: EditClassFormProps) {
                     render={({ field }) => (
                     <FormItem>
                         <FormLabel>Local</FormLabel>
-                        <Select onValueChange={field.onChange} defaultValue={field.value}>
+                        <Select onValueChange={field.onChange} value={field.value}>
                         <FormControl>
                             <SelectTrigger>
                             <SelectValue placeholder="Selecione..." />
